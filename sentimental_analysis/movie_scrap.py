55cab7692a16790dd7b4d9e75546eab915be7959
PushpendraPathak <69740771+PushpendraPathak@users.noreply.github.com>
2020-10-26 19:12:03 -0400

    Update movie_scrap.py

--- sentimental_analysis/movie_scrap.py
+++ sentimental_analysis/movie_scrap.py
@@ -7,14 +7,19 @@ if int(variable)==1:
   URL="https://www.imdb.com/title/tt10970552/reviews?ref_=tt_urv"
   r=requests.get(URL)
   soup=BeautifulSoup(r.content)
-  gg=soup.findAll('div', {'class' : 'text show-more__control'}, limit=None)
-  print(gg)
+  result=soup.findAll('div', {'class' : 'text show-more__control'}, limit=None)
+ 
 if int(variable)==2:
   abc=""
   URL="https://www.rottentomatoes.com/m/on_the_rocks_2020/reviews"
-  r=requests.get(URL)
-  soup=BeautifulSoup(r.content)
-  gg=soup.findAll('div', {'class' : 'the_review'}, limit=None)
-  for item in gg:
-    abc=abc+(item.text)
-  print(abc)
+  
+  for i in range(25):
+    tempURL=URL+"?type=&sort=&page="+str(i)
+    print(tempURL)
+    r=requests.get(tempURL)
+    soup=BeautifulSoup(r.content)
+    result=soup.findAll('div', {'class' : 'the_review'}, limit=None)
+    for item in result:
+      abc=abc+(item.text)
+  result=abc
+return result
